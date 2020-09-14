8c193bd27a1e0123e1e2094db82dea7f6749792a
Plvltra <331079072@qq.com>
2020-09-15 01:17:10 +0800

    finish2

--- Assets/Scripts/BallSword.cs
+++ Assets/Scripts/BallSword.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class BallSword : Ball
 {
 	// Start is called before the first frame update
-	// 向前攻击的角度(弧度制)
+	// 向前攻击的角度(角度制)
 	public float maxForwardAngle;
 	public float attackSpead;
 
@@ -22,7 +23,8 @@ public class BallSword : Ball
 	// Update is called once per frame
 	protected override void Update()
     {
-		base.Update();
+		//base.Update();
+		DeathCheck();
 		if (isAlive && !isSelling && gameController.gamePhase == GameController.GamePhase.Playing)
 		{
 			RotateSword();
@@ -46,6 +48,35 @@ public class BallSword : Ball
 		bias = forward ? bias : -bias;
 		currentAngle += bias;
 		swordTrans.RotateAround(point, Vector3.back, bias);
+	}
+
+	protected new void DeathCheck()
+	{
+		if (isAlive)
+		{
+			if (health <= 0)
+			{
+				StartCoroutine(Die());
+			}
+		}
+	}
+
+	protected override IEnumerator Die()
+	{
+		isAlive = false;
+		// 剑与球分离，自由降落
+		GameObject childGameObject = transform.GetChild(0).gameObject;
+		Destroy(childGameObject.GetComponent<Collider2D>());
+		childGameObject.transform.parent = null;
+
+		// 球旋转自由降落
+		Destroy(gameObject.GetComponent<Collider2D>());
+		body.AddTorque(torqueDeath);
+
+		// 等待死亡持续时间
+		yield return new WaitForSeconds(3);
+		Destroy(childGameObject);
+		Destroy(gameObject);
 	}
 
 }
