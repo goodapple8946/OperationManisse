0d8539842e81d1d70c9133dac69ee665053f5fa1
Plvltra <331079072@qq.com>
2020-09-15 18:02:36 +0800

    update

--- Assets/Scripts/BallSword.cs
+++ Assets/Scripts/BallSword.cs
@@ -9,27 +9,23 @@ public class BallSword : Ball
 	// 向前攻击的角度(角度制)
 	public float maxForwardAngle;
 	public float attackSpead;
+	public float liftSwordSpeed;
 
 	// 是否向前
 	private bool forward = true;
 	// 相对于初始位置偏移角度
 	private float currentAngle = 0.0f;
 
-	//protected override void Start()
-	//{
-	//	base.Start();
-	//}
-
 	// Update is called once per frame
 	protected override void Update()
-    {
+	{
 		//base.Update();
 		DeathCheck();
 		if (isAlive && !isSelling && gameController.gamePhase == GameController.GamePhase.Playing)
 		{
 			RotateSword();
 		}
-    }
+	}
 
 	private void RotateSword()
 	{
@@ -44,39 +40,31 @@ public class BallSword : Ball
 		{
 			forward = true;
 		}
-		float bias = attackSpead * Time.deltaTime;
-		bias = forward ? bias : -bias;
+		float factor = forward ? attackSpead : -liftSwordSpeed;
+		float bias = factor * Time.deltaTime;
 		currentAngle += bias;
 		swordTrans.RotateAround(point, Vector3.back, bias);
 	}
 
 	protected new void DeathCheck()
 	{
-		if (isAlive)
+		if (health <= 0)
 		{
-			if (health <= 0)
-			{
-				StartCoroutine(Die());
-			}
+			isAlive = false;
+			// 剑与球分离，自由降落
+			GameObject childObject = transform.GetChild(0).gameObject;
+			childObject.GetComponent<Collider2D>().enabled = false;
+
+			// 剑添加关节到ball
+			FixedJoint2D joint = childObject.AddComponent<FixedJoint2D>();
+			joint.connectedBody = body;
+			joint.enableCollision = false;
+
+			// 球旋转自由降落
+			gameObject.GetComponent<Collider2D>().enabled = false;
+			body.AddTorque(torqueDeath);
+			Destroy(gameObject, deathDuration);
 		}
-	}
-
-	protected override IEnumerator Die()
-	{
-		isAlive = false;
-		// 剑与球分离，自由降落
-		GameObject childGameObject = transform.GetChild(0).gameObject;
-		Destroy(childGameObject.GetComponent<Collider2D>());
-		childGameObject.transform.parent = null;
-
-		// 球旋转自由降落
-		Destroy(gameObject.GetComponent<Collider2D>());
-		body.AddTorque(torqueDeath);
-
-		// 等待死亡持续时间
-		yield return new WaitForSeconds(3);
-		Destroy(childGameObject);
-		Destroy(gameObject);
 	}
 
 }
