d97c7d4fc53064e033170201c94d393aedca565f
Plvltra <331079072@qq.com>
2020-09-29 20:10:59 +0800

    commit

--- Assets/Scripts/UI/UI Editor/EditorSave.cs
+++ Assets/Scripts/UI/UI Editor/EditorSave.cs
@@ -10,7 +10,12 @@ public class EditorSave : MonoBehaviour
 	public void SaveFile()
 	{
 		string filename = Path.Combine(Application.dataPath, "1.xml");
+		if(editorController.Backgrounds == null)
+		{
+			Debug.Log(123);
+		}
 		List<Background> backgrounds = editorController.Backgrounds.OfType<Background>().ToList();
+
 
 		List<Unit> units = editorController.Grid.OfType<Unit>().ToList();
 		foreach(Unit unit in units)
