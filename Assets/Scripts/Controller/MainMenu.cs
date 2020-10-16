23a2b6745696887d1e1f8f1300d24db7af10cd5a
牛顿炖牛肉 <goodapple8946@163.com>
2020-10-16 10:29:20 +0800

    2020-10-16 10:29

---
+++ Assets/Scripts/Controller/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Story()
+    {
+
+    }
+
+    public void Custom()
+    {
+
+    }
+
+    public void Editor()
+    {
+        SceneManager.LoadScene("Scene Editor");
+    }
+
+    public void Option()
+    {
+
+    }
+
+    public void Credits()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
